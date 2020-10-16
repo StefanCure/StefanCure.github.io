85fa9c3ed62fbc3586cd75ef7a287cc4e4b50390
Cure Stefan <akpanowoprecious@gmail.com>
2020-10-16 11:20:12 +0100

    added a new web folder

---
+++ fylo-dark-theme-landing-page-master/app.js
@@ -0,0 +1,10 @@
+
+
+  
+  ScrollReveal().reveal('.headline', { distance: '-15px', delay:500, duration: 1000, easing: 'ease-in' });
+  ScrollReveal().reveal('.headline1', { delay: 1500, distance: '-15px', duration: 1000, easing: 'ease-in'});
+  //ScrollReveal().reveal('.headline2', { duration: 2000, scale: 0.7, distance: '-20%', easing: 'ease-in', opacity: 0.7 });
+  ScrollReveal().reveal('.headline2', { duration: 3000, distance: '40%', easing: 'ease-in', opacity: 0.7,  
+  viewOffset: {
+      top: 500
+  }});
